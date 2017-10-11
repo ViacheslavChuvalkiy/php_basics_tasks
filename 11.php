502e2ee0493a299e8f9f3e4e6691d332b378b1d1
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-10-11 14:06:31 +0300

    Update 11.php
    Ваш вариант тоже рабочий, но так короче

--- 11.php
+++ 11.php
@@ -1,13 +1,18 @@
 <?php
+
 $day = random_int ( 1 , 7 );
 echo $day;
 
 switch ($day) {
-    case 1: echo ' Это рабочий день '; break;
-    case 2: echo ' Это рабочий день '; break;
-    case 3: echo ' Это рабочий день '; break;
-    case 4: echo ' Это рабочий день '; break;
-    case 5: echo ' Это рабочий день '; break;
-    case 6: echo ' Это выходной день'; break;
-    case 7: echo ' Это выходной день '; break;
+    case 1: 
+    case 2: 
+    case 3: 
+    case 4: 
+    case 5: 
+        echo ' Это рабочий день '; 
+        break;
+    case 6:
+    case 7: 
+        echo ' Это выходной день '; 
+        break;
 }
